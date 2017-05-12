ad36df2e16f7f7d142fdeed3370b090f26c77144
vincent.bai@sap.com <vincent.bai@sap.com>
2017-05-12 00:41:53 +0000

    12/05/

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,6 +6,16 @@ sap.ui.define(
 	function(Controller, MessageToast) {
 
 		return Controller.extend("opensap.myapp.controller.App", {
+			onInit:function(oEvent){
+			this._oViewModel = new sap.ui.model.json.JSONModel({
+                "NoOfPicking":1,
+                "PickingTotal": 5,
+                "PickingIndicatorStatus": "Success"
+            });
+
+            this.getView().setModel(this._oViewModel, "vm" );
+			},
+			
 			onButtonSayHello: function() {
 				jQuery.sap.require("sap.m.MessageBox");
 				sap.m.MessageBox.show("This message should appear in the message box.", {
@@ -23,6 +33,11 @@ sap.ui.define(
 					duration: 3000
 				});
 
+			},
+			
+			onImageLoadError: function(oEvent) {
+				var oControl = oEvent.getSource();
+				oControl.setSrc("http://qa-test-fsnz-images.s3.amazonaws.com/FAN/5001976.png");
 			}
 		});
 	});
